4fe06e7d0afacc25508d0aba47ccb3dd3a4fbe52
NandhiniTR007 <nandhini.rajkumar@templeray.io>
2025-02-12 01:06:31 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(bodyParser.json());
 // Define a base route
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is my first Node.js app. This changes is triggered by push event. I think Finally its working.');
+    res.json('Hello, World! This is my first Node.js app. This changes is triggered by push event. I think Finally its working.');
 });
 
 // Start the server
