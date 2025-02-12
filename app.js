9375655a7f3b53c1b5ecfcb31756b02f8fb896e7
NandhiniTR007 <nandhini.rajkumar@templeray.io>
2025-02-13 00:53:14 +0530

    Addes cron job

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { CronJob } = require('cron');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,6 +8,14 @@ app.use(bodyParser.json());
 // Define a base route
 app.get('/', (req, res) => {
     res.json('Hello, World! This is my first Node.js app. This changes is triggered by push event. I think Finally its working.');
+});
+const job = CronJob.from({
+	cronTime: '* * * * *',
+	onTick: function () {
+		res.json('You will see this message every minute');
+	},
+	start: true,
+	timeZone: 'America/Los_Angeles'
 });
 
 // Start the server
